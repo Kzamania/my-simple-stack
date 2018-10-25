05083cc3739a960ccaedd2c54045ef620b8cd869
Maxime DURAND <maxime.durand1@univ-tlse3.fr>
2018-10-25 11:06:32 +0200

    First commit

---
+++ src/main/java/deqo/mdur/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.mdur.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
